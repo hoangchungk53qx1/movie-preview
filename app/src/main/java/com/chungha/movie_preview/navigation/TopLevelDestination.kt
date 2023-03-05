4f6d2ecc31e254a91fb77ed8727c97df57fd989d
hoangchungk53qx1 <hoangchungk53qx1@gmail.com>
2023-03-05 14:03:38 +0700

    add multiple module

---
+++ app/src/main/java/com/chungha/movie_preview/navigation/TopLevelDestination.kt
@@ -0,0 +1,35 @@
+package com.chungha.movie_preview.navigation
+
+import android.graphics.drawable.Icon
+
+enum class TopLevelDestination(
+    val selectedIcon: Icon,
+    val unselectedIcon: Icon,
+    val iconTextId: Int,
+    val titleTextId: Int,
+) {
+//    Home(
+//        route = HomeDestination.route,
+//        destination = HomeDestination.destination,
+//        iconResourceId = R.drawable.ic_home,
+//        textResourceId = R.string.home
+//    ),
+//    Search(
+//        route = SearchDestination.route,
+//        destination = SearchDestination.destination,
+//        iconResourceId = R.drawable.ic_search,
+//        textResourceId = R.string.search
+//    ),
+//    Wishlist(
+//        route = WishlistDestination.route,
+//        destination = WishlistDestination.destination,
+//        iconResourceId = R.drawable.ic_wishlist,
+//        textResourceId = R.string.wishlist
+//    ),
+//    Settings(
+//        route = SettingsDestination.route,
+//        destination = SettingsDestination.destination,
+//        iconResourceId = R.drawable.ic_settings,
+//        textResourceId = R.string.settings
+//    )
+}
---
+++ core/core-common/src/main/java/com/example/core_common/MyClass.kt
@@ -0,0 +1,4 @@
+package com.example.core_common
+
+class MyClass {
+}
